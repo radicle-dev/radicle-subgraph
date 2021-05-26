23a515cfd55c3ec173ea9e0a906ace9370cf54c0
Alexis Sellier <self@cloudhead.io>
2021-05-26 16:56:51 +0200

    Save org address in Anchor

--- src/org.ts
+++ src/org.ts
@@ -1,5 +1,6 @@
 import { OrgV1, Anchored, Unanchored } from "../generated/OrgV1Factory/OrgV1";
 import { Org, Anchor } from "../generated/schema";
+import { Address } from '@graphprotocol/graph-ts'
 
 export function handleAnchored(event: Anchored): void {
   let anchor = new Anchor(event.transaction.hash.toHex());
@@ -8,6 +9,7 @@ export function handleAnchored(event: Anchored): void {
   anchor.stateHash = event.params.hash;
   anchor.stateHashFormat = event.params.format;
   anchor.objectType = event.params.kind;
+  anchor.orgAddress = event.address as Address;
 
   anchor.save();
 }
