665a1757c59ed36fe58a3a1b6cdbeef11708f1bc
Alexis Sellier <self@cloudhead.io>
2021-05-26 17:40:14 +0200

    Add Project entity

--- src/org.ts
+++ src/org.ts
@@ -1,5 +1,5 @@
 import { OrgV1, Anchored, Unanchored } from "../generated/OrgV1Factory/OrgV1";
-import { Org, Anchor } from "../generated/schema";
+import { Org, Anchor, Project } from "../generated/schema";
 import { Address } from '@graphprotocol/graph-ts'
 
 export function handleAnchored(event: Anchored): void {
@@ -12,6 +12,14 @@ export function handleAnchored(event: Anchored): void {
   anchor.orgAddress = event.address as Address;
 
   anchor.save();
+
+  if (event.params.kind === 0x0) {
+    let proj = new Project(event.params.id.toHex());
+    proj.org = event.address.toHex();
+    proj.stateHash = anchor.stateHash;
+    proj.stateHashFormat = anchor.stateHashFormat;
+    proj.save();
+  }
 }
 
 export function handleUnanchored(event: Unanchored): void {}
